6c1633041dd1f4ccc8c45e7be0ea472fb127a517
Eli Pacheco <elipacheco@elis-air.lan>
2022-07-18 19:14:58 -0600

    fixed abs

--- Q3/sdvd.py
+++ Q3/sdvd.py
@@ -3,11 +3,17 @@
 import math
 
 
+def mu_d_sample(mod,sdod,mu_d,n):
+    degree_freedom = n-1
+    test_statistic = (abs(mod) - mu_d)/(sdod/math.sqrt(n))
+    print(f'test statistic, t = {test_statistic}')
+    print(f'The degree of freedom is: {degree_freedom}')
 
-def sample_mean_difference(x,y,n):
-    
+def sample_mean_difference(x,y,mu_d):
+    n = len(x)
     meanlist = []
     sdlist = []
+    degree_freedom = n-1
     for i,j in zip(x,y):
         num0 = i-j
         meanlist.append(num0)
@@ -18,7 +24,34 @@ def sample_mean_difference(x,y,n):
         sdlist.append(num2)
     sample_variance = sum(sdlist)/(len(sdlist)-1)
     standard_deviation_of_difference = math.sqrt(sample_variance)
-    print(f'The mean of difference is: {mean_of_difference}')
-    print(f'The standard deviaion of difference is: {standard_deviation_of_difference}')
-    test_statistic = mean_of_difference/(standard_deviation_of_difference/math.sqrt(n))
-    print(f'test statistic, t = {test_statistic}')
+    if mu_d != 0:
+        mu_d_sample(mean_of_difference,standard_deviation_of_difference,mu_d,n)
+    else:
+        test_statistic = (mean_of_difference - mu_d)/(standard_deviation_of_difference/math.sqrt(n))
+        print(f'test statistic, t = {test_statistic}')
+        print(f'The degree of freedom is: {degree_freedom}')
+
+
+a = [394,
+513,
+483,
+447,
+440,
+473,
+440,
+481,
+459,
+399]
+b = [430,
+525,
+482,
+482,
+479,
+493,
+466,
+482,
+455,
+404]
+
+mu_d = 15
+sample_mean_difference(a,b,mu_d)
