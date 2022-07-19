d4f515718f5a646f035a36e38609c6bbf656c069
Eli Pacheco <elipacheco@elis-air.lan>
2022-07-18 19:58:00 -0600

    fixed lists

--- Q3/h_test.py
+++ Q3/h_test.py
@@ -55,7 +55,41 @@ def test_statistict_t(x1,x2,mu1,mu2,s1,s2,n1,n2):
     insquare = (s1**2/n1)+(s2**2/n2)
     denom = math.sqrt(insquare)
     t = nom/denom
+    if n1 < n2:
+        degrees_of_freedom = n1-1
+    else:
+        degrees_of_freedom = n2-1
     print(f't = {t}')
+    print(f'Degrees of freedom are: {degrees_of_freedom}')
+
+def test_statistict_tlist(a,b,mu1,mu2):
+    n1 = len(a)
+    n2 = len(b)
+    x1 = sum(a)/len(a)
+    x2 = sum(b)/len(b)
+    s1list = []
+    s2list = []
+    for i,j in zip(a,b):
+        val1 = abs(i-x1)
+        val1 = val1**2
+        s1list.append(val1)
+        val2 = abs(j-x2)
+        val2 = val2**2
+        s2list.append(val2)
+    sv1 = sum(s1list)/(len(s1list)-1)
+    sv2 = sum(s2list)/(len(s2list)-1)
+    s1 = math.sqrt(sv1)
+    s2 = math.sqrt(sv2)
+    nom = (x1-x2)-(mu1-mu2)
+    insquare = (s1**2/n1)+(s2**2/n2)
+    denom = math.sqrt(insquare)
+    t = nom/denom
+    if n1 < n2:
+        degrees_of_freedom = n1-1
+    else:
+        degrees_of_freedom = n2-1
+    print(f't = {t}')
+    print(f'Degrees of freedom are: {degrees_of_freedom}')
 
 def test_statistic_z(p0,p1,p2,n1,n2):
     nom = p1-p2 
@@ -92,9 +126,4 @@ def two_tailed(h0,n,x,sigma,alpha1,alpha2):
 
 # for this program, if h1 > h0  or h1 < h0, NOTE: make h1 = h0
 # the program will compute the rest
-
-
-p = pooled_proportion(45,22,337,88)
-p1 = sample_proportion(45,337)
-p2 = sample_proportion(22,88)
-test_statistic_z(p,p1,p2,337,88)
+test_statistict_t(7.2,4.2,0,0,2.8,2.5,5,7)
